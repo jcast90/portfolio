4e41f562256a080d17280f5fc3f9f3bafbb9c68c
Jonathan Lancaster <jcast90@me.com>
2016-03-02 13:07:58 -0800

    Add Files

---
+++ js/jquery.js
@@ -0,0 +1,18 @@
+/* Saving for when I can get this to work properly for final.
+  
+setInterval(function() {
+  $('#bounce').effect('bounce',1000)
+  }, 1000);
+  
+
+$('#up').click(function(){
+			console.log("UP");
+		$('#down').toggleClass('toggle');
+		$('#up').toggleClass('toggle');
+		});
+		
+		$('#down').click(function(){
+			console.log("DOWN");
+		$('#down').toggleClass('toggle');
+		$('#up').toggleClass('toggle');
+			}); */
